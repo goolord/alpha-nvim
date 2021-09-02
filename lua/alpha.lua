2c44c66ffb0177e550843b6bff3dab15e2e9bfba
Zachary Churchill <zacharyachurchill@gmail.com>
2021-09-01 20:52:39 -0400

    wrapping, padding

--- lua/alpha.lua
+++ lua/alpha.lua
@@ -11,20 +11,24 @@ local default_header = {
     opts = {
         position = "center";
         hl = "Type";
+        -- wrap = "overflow";
     };
 }
+
 local default_opts = {
     layout = {
+        { type = "padding"; val = 2 };
         default_header;
-        { type = "padding"; val = 10 };
+        { type = "padding"; val = 5 };
         {
             type = "text";
             val = "foo";
             opts = {
-                position = "center"
+                -- position = "center"
             };
         };
-    }
+    };
+    margin = 5;
 }
 
 local options = default_opts
@@ -49,49 +53,88 @@ function center(tbl, state)
     return centered
 end
 
-function layout(layout_tbl, state)
+function pad_margin(tbl, state, margin)
+    local longest = longest_line(tbl)
+    local pot_width = margin + margin + longest
+    local win_width = vim.api.nvim_win_get_width(state.window)
+    local left 
+    if pot_width > win_width then
+        left = (win_width - pot_width) + margin
+    else
+        left = margin
+    end
+    local padding = string.rep(" ", left)
+    local padded = {}
+    for k,v in pairs(tbl) do
+        padded[k] = padding..v..padding
+    end
+    return padded
+end
+
+-- function trim(tbl, state)
+--     local win_width = vim.api.nvim_win_get_width(state.window)
+--     local trimmed = {}
+--     for k,v in pairs(tbl) do
+--         trimmed[k] = string.sub(v, 1, win_width)
+--     end
+--     return trimmed
+-- end
+
+function layout(opts, state)
     local layout_element = {
-        ["text"] = function (opts)
-            if type(opts.val) == "table" then
-                local end_ln = state.line + #opts.val
-                local val = opts.val
-                if opts.opts.position == "center" then val = center(val, state) end
+        ["text"] = function (el)
+            if type(el.val) == "table" then
+                local end_ln = state.line + #el.val
+                local val = el.val
+                if opts.margin then val = pad_margin(val, state, opts.margin) end
+                if el.opts.position == "center" then val = center(val, state) end
+                -- if el.opts.wrap == "overflow" then
+                --     val = trim(val, state)
+                -- end
                 vim.api.nvim_buf_set_lines(state.buffer, state.line, state.line, true, val)
-                if opts.opts.hl then
+                if el.opts.hl then
                     for i = state.line, end_ln do
-                        vim.api.nvim_buf_add_highlight(state.buffer, -1, opts.opts.hl, i , 0 , -1)
+                        vim.api.nvim_buf_add_highlight(state.buffer, -1, el.opts.hl, i , 0 , -1)
                     end
                 end
                 state.line = end_ln
             end
-            if type(opts.val) == "string" then
-                local val = { opts.val }
+            if type(el.val) == "string" then
+                local val = { el.val }
+                if opts.margin then val = pad_margin(val, state, opts.margin) end
+                if el.opts.position == "center" then val = center(val, state) end
                 vim.api.nvim_buf_set_lines(state.buffer, state.line, state.line, true, val)
+                if el.opts.hl then
+                    vim.api.nvim_buf_add_highlight(state.buffer, -1, el.opts.hl, state.line , 0 , -1)
+                end
                 state.line = state.line + 1
             end
         end,
-        ["padding"] = function (opts)
-            local end_ln = state.line + opts.val
+
+        ["padding"] = function (el)
+            local end_ln = state.line + el.val
             local val = { }
-            for i = 1, opts.val + 1 do
+            for i = 1, el.val + 1 do
                 val[i] = ""
             end
             vim.api.nvim_buf_set_lines(state.buffer, state.line, state.line, true, val)
             state.line = end_ln
         end,
-        ["button"] = function (opts)
+
+        ["button"] = function (el)
         end,
     }
 
     local line_state = 0
-    for _,opts in pairs(layout_tbl) do
-        layout_element[opts.type](opts, state)
+    for _,el in pairs(opts.layout) do
+        layout_element[el.type](el, state)
     end
 end
 
+function _G.alpha_redraw() end
+
 function start(opts)
     opts = opts or options
-    vim.opt_local.modifiable = true
 
     local buffer = vim.api.nvim_create_buf(true, true)
     vim.api.nvim_set_current_buf(buffer)
@@ -103,9 +146,15 @@ function start(opts)
     }
 
     enable_alpha()
-    layout(opts.layout, state)
-
-    vim.opt_local.modifiable = false
+    local draw = function ()
+        vim.api.nvim_buf_set_option(state.buffer, 'modifiable', true)
+        vim.api.nvim_buf_set_lines(state.buffer, 0, state.line, false, {})
+        state.line = 0
+        layout(opts, state)
+        vim.api.nvim_buf_set_option(state.buffer, 'modifiable', false)
+    end
+    _G.alpha_redraw = draw
+    draw()
 end
 
 function enable_alpha()
@@ -123,12 +172,17 @@ function enable_alpha()
     vim.opt_local.swapfile       = false
     vim.opt_local.signcolumn     = 'no'
     vim.opt_local.synmaxcol      = vim.api.nvim_get_option_info('synmaxcol').default
+    vim.opt_local.wrap           = false
 
     vim.opt_local.ft = 'alpha'
 end
 
 function setup(opts)
     vim.cmd("command Alpha lua require'alpha'.start()")
+    vim.cmd([[augroup alpha]])
+    vim.cmd([[au!]])
+    vim.cmd([[autocmd VimResized * if &filetype ==# 'alpha' | call v:lua.alpha_redraw() | endif]])
+    vim.cmd([[augroup END]])
     if type(opts) == "table" then
         options = opts
     end
