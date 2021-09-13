d6afd8b36ece8358b2f5d22e6b5f5e0556c3ca10
Zachary Churchill <zacharyachurchill@gmail.com>
2021-09-13 16:44:16 -0400

    dont # fn_start until checked

--- lua/alpha/themes/startify.lua
+++ lua/alpha/themes/startify.lua
@@ -72,9 +72,9 @@ local function file_button(fn, sc, short_fn)
             ico_txt = ''
     end
     local file_button_el = button(sc, ico_txt .. short_fn , ":e " .. fn .. " <CR>")
-    local fn_start = #short_fn:match(".*/")
+    local fn_start = short_fn:match(".*/")
     if fn_start ~= nil then
-        table.insert(fb_hl, {"Comment", #ico_txt - 2, fn_start + #ico_txt - 2})
+        table.insert(fb_hl, {"Comment", #ico_txt - 2, #fn_start + #ico_txt - 2})
     end
     file_button_el.opts.hl = fb_hl
     return file_button_el
