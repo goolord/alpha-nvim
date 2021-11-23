b253f3feb52e22e8b6528d66bd90d49f7b945bc2
Zachary Churchill <zacharyachurchill@gmail.com>
2021-11-23 15:46:16 -0500

    use stylua, add editorconfig file

--- lua/alpha/themes/startify.lua
+++ lua/alpha/themes/startify.lua
@@ -16,7 +16,7 @@ local default_header = {
         hl = "Type",
         shrink_margin = false,
         -- wrap = "overflow";
-    }
+    },
 }
 
 --- @param sc string
@@ -28,20 +28,20 @@ local function button(sc, txt, keybind, keybind_opts)
 
     local opts = {
         position = "left",
-        shortcut = "["..sc.."] ",
+        shortcut = "[" .. sc .. "] ",
         cursor = 1,
         -- width = 50,
         align_shortcut = "left",
-        hl_shortcut = { {"Operator", 0, 1}, {"Number", 1, #sc+1}, {"Operator", #sc+1, #sc+2} },
+        hl_shortcut = { { "Operator", 0, 1 }, { "Number", 1, #sc + 1 }, { "Operator", #sc + 1, #sc + 2 } },
         shrink_margin = false,
     }
     if keybind then
-        keybind_opts = if_nil(keybind_opts, {noremap = true, silent = true, nowait = true})
-        opts.keymap = {"n", sc_, keybind, {noremap = false, silent = true, nowait = true}}
+        keybind_opts = if_nil(keybind_opts, { noremap = true, silent = true, nowait = true })
+        opts.keymap = { "n", sc_, keybind, { noremap = false, silent = true, nowait = true } }
     end
 
-    local function on_press ()
-        local key = vim.api.nvim_replace_termcodes(sc_ .. '<Ignore>', true, false, true)
+    local function on_press()
+        local key = vim.api.nvim_replace_termcodes(sc_ .. "<Ignore>", true, false, true)
         vim.api.nvim_feedkeys(key, "normal", false)
     end
 
@@ -49,18 +49,18 @@ local function button(sc, txt, keybind, keybind_opts)
         type = "button",
         val = txt,
         on_press = on_press,
-        opts = opts
+        opts = opts,
     }
 end
 
 local nvim_web_devicons = {
     enabled = true,
-    highlight = true
+    highlight = true,
 }
 
 local function get_extension(fn)
     local match = fn:match("^.+(%..+)$")
-    local ext = ''
+    local ext = ""
     if match ~= nil then
         ext = match:sub(2)
     end
@@ -68,7 +68,7 @@ local function get_extension(fn)
 end
 
 local function icon(fn)
-    local nwd = require('nvim-web-devicons')
+    local nwd = require("nvim-web-devicons")
     local ext = get_extension(fn)
     return nwd.get_icon(fn, ext, { default = true })
 end
@@ -77,37 +77,36 @@ local function file_button(fn, sc, short_fn)
     short_fn = if_nil(short_fn, fn)
     local ico_txt
     local fb_hl = {}
-    if nvim_web_devicons.enabled
-        then
-            local ico, hl = icon(fn)
-            local hl_option_type = type(nvim_web_devicons.highlight)
-            if hl_option_type == "boolean" then
-                if hl and nvim_web_devicons.highlight then table.insert(fb_hl, { hl, 0, 1 }) end
+    if nvim_web_devicons.enabled then
+        local ico, hl = icon(fn)
+        local hl_option_type = type(nvim_web_devicons.highlight)
+        if hl_option_type == "boolean" then
+            if hl and nvim_web_devicons.highlight then
+                table.insert(fb_hl, { hl, 0, 1 })
             end
-            if hl_option_type == "string" then
-                table.insert(fb_hl, {nvim_web_devicons.highlight , 0, 1 })
-            end
-            ico_txt = ico .. '  '
-        else
-            ico_txt = ''
+        end
+        if hl_option_type == "string" then
+            table.insert(fb_hl, { nvim_web_devicons.highlight, 0, 1 })
+        end
+        ico_txt = ico .. "  "
+    else
+        ico_txt = ""
     end
-    local file_button_el = button(sc, ico_txt .. short_fn , ":e " .. fn .. " <CR>")
+    local file_button_el = button(sc, ico_txt .. short_fn, ":e " .. fn .. " <CR>")
     local fn_start = short_fn:match(".*/")
     if fn_start ~= nil then
-        table.insert(fb_hl, {"Comment", #ico_txt - 2, #fn_start + #ico_txt - 2})
+        table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt - 2 })
     end
     file_button_el.opts.hl = fb_hl
     return file_button_el
 end
 
-local default_mru_ignore = { 'gitcommit' }
+local default_mru_ignore = { "gitcommit" }
 
 local mru_opts = {
     ignore = function(path, ext)
-        return
-                (string.find(path, "COMMIT_EDITMSG"))
-            or  (vim.tbl_contains(default_mru_ignore, ext))
-    end
+        return (string.find(path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
+    end,
 }
 
 --- @param start number
@@ -117,34 +116,37 @@ local function mru(start, cwd, items_number, opts)
     opts = opts or mru_opts
     items_number = if_nil(items_number, 10)
     local oldfiles = {}
-    for _,v in ipairs(vim.v.oldfiles) do
-        if #oldfiles == items_number then break end
+    for _, v in ipairs(vim.v.oldfiles) do
+        if #oldfiles == items_number then
+            break
+        end
         local cwd_cond
-        if not cwd
-            then cwd_cond = true
-            else cwd_cond = vim.startswith(v, cwd)
+        if not cwd then
+            cwd_cond = true
+        else
+            cwd_cond = vim.startswith(v, cwd)
         end
         local ignore = (opts.ignore and opts.ignore(v, get_extension(v))) or false
-        if (filereadable(v) == 1) and cwd_cond and (not ignore) then
-            oldfiles[#oldfiles+1] = v
+        if (filereadable(v) == 1) and cwd_cond and not ignore then
+            oldfiles[#oldfiles + 1] = v
         end
     end
 
     local tbl = {}
     for i, fn in ipairs(oldfiles) do
         local short_fn
-        if cwd
-            then short_fn = fnamemodify(fn, ':.')
-            else short_fn = fnamemodify(fn, ':~')
+        if cwd then
+            short_fn = fnamemodify(fn, ":.")
+        else
+            short_fn = fnamemodify(fn, ":~")
         end
-        local file_button_el = file_button(fn, tostring(i+start-1), short_fn)
+        local file_button_el = file_button(fn, tostring(i + start - 1), short_fn)
         tbl[i] = file_button_el
     end
     return {
         type = "group",
         val = tbl,
-        opts = {
-        }
+        opts = {},
     }
 end
 
@@ -158,7 +160,7 @@ local section = {
         type = "group",
         val = {
             button("e", "New file", ":ene <CR>"),
-        }
+        },
     },
     -- note about MRU: currently this is a function,
     -- since that means we can get a fresh mru
@@ -170,53 +172,63 @@ local section = {
     mru = {
         type = "group",
         val = {
-            {type = "padding", val = 1},
-            {type = "text", val = "MRU", opts = { hl = "SpecialComment" }},
-            {type = "padding", val = 1},
-            {type = "group", val = function() return { mru(0) } end},
-        }
+            { type = "padding", val = 1 },
+            { type = "text", val = "MRU", opts = { hl = "SpecialComment" } },
+            { type = "padding", val = 1 },
+            {
+                type = "group",
+                val = function()
+                    return { mru(0) }
+                end,
+            },
+        },
     },
     mru_cwd = {
         type = "group",
         val = {
-            {type = "padding", val = 1},
-            {type = "text", val = mru_title , opts = { hl = "SpecialComment", shrink_margin = false}},
-            {type = "padding", val = 1},
-            {type = "group", val = function() return { mru(10, vim.fn.getcwd()) } end, opts = {shrink_margin = false}},
-        }
+            { type = "padding", val = 1 },
+            { type = "text", val = mru_title, opts = { hl = "SpecialComment", shrink_margin = false } },
+            { type = "padding", val = 1 },
+            {
+                type = "group",
+                val = function()
+                    return { mru(10, vim.fn.getcwd()) }
+                end,
+                opts = { shrink_margin = false },
+            },
+        },
     },
     bottom_buttons = {
         type = "group",
         val = {
             button("q", "Quit", ":q <CR>"),
-        }
+        },
     },
     footer = {
         type = "group",
-        val = {
-        }
+        val = {},
     },
 }
 
 local opts = {
     layout = {
-        {type = "padding", val = 1},
+        { type = "padding", val = 1 },
         section.header,
-        {type = "padding", val = 2},
+        { type = "padding", val = 2 },
         section.top_buttons,
         section.mru,
         section.mru_cwd,
-        {type = "padding", val = 1},
+        { type = "padding", val = 1 },
         section.bottom_buttons,
         section.footer,
     },
     opts = {
         margin = 3,
         redraw_on_resize = false,
-        setup = function ()
-            vim.cmd[[
+        setup = function()
+            vim.cmd([[
             autocmd alpha_temp DirChanged * call v:lua.alpha_redraw()
-            ]]
+            ]])
         end,
     },
 }
