b0f1744be62bd4f396989479cb8e39e62d982eca
Mark Maglana <mmaglana@gmail.com>
2023-07-15 16:16:38 +0800

    Reposition the term as needed
    This change adds logic that allows the terminal window to reposition
    accordingly when a window split occurs. For example, when neotree is
    made visible while alpha is also visible.

--- lua/alpha/term.lua
+++ lua/alpha/term.lua
@@ -3,13 +3,16 @@ local alpha = require("alpha")
 local M = {}
 
 function M.open_window(el)
+    local parent_win = vim.api.nvim_get_current_win()
+    local parent_win_width = vim.api.nvim_win_get_width(parent_win)
     local width = el.width
     local height = el.height
     local row = math.floor(height / 5)
-    local col = math.floor((vim.o.columns - width) / 2)
+    local col = math.floor((parent_win_width - width) / 2)
 
     local opts = vim.tbl_extend("keep", (el.opts and el.opts.window_config) or {}, {
-        relative = "editor",
+        relative = "win",
+        win = parent_win,
         row = row,
         col = col,
         width = width,
@@ -56,10 +59,43 @@ vim.api.nvim_create_autocmd("User", {
     end,
 })
 
+function M.find_parent_id()
+    local windows = vim.api.nvim_list_wins()
+    for _, winid in ipairs(windows) do
+        local success, value = pcall(vim.api.nvim_win_get_var, winid, "alpha_section_terminal")
+        if success and value then
+            return winid
+        end
+    end
+    return nil
+end
+
+function M.reposition()
+    local parent_id = M.find_parent_id()
+    if parent_id == nil then
+        return
+    end
+
+    local parent_width = vim.api.nvim_win_get_width(parent_id)
+    local position = vim.api.nvim_win_get_position(parent_id)
+    local parent_col = position[2]
+    local parent_row = position[1]
+
+    local term_info = vim.api.nvim_win_get_var(parent_id, "alpha_section_terminal")
+    local term_id = term_info[2]
+    local config = vim.api.nvim_win_get_config(term_id)
+    config.row = math.floor(config.height / 5 + parent_row)
+    config.col = math.floor((parent_width - config.width) / 2 + parent_col)
+
+    vim.api.nvim_win_set_config(term_id, config)
+end
+
 function alpha.layout_element.terminal(el, conf, state)
     if el.opts and (el.opts.redraw == nil or el.opts.redraw) then
         el.opts.redraw = false
         M.run_command(el.command, el)
+    else
+        M.reposition()
     end
     return alpha.layout_element.padding({ type = "padding", val = el.height }, conf, state)
 end
