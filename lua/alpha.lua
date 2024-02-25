140e3b96197c8d0d8e91e9516b4e0e1855fcb30b
Zachary Churchill <zacharyachurchill@gmail.com>
2024-02-25 14:59:26 -0500

    revert

--- lua/alpha.lua
+++ lua/alpha.lua
@@ -502,9 +502,14 @@ local function enable_alpha(conf, state)
 
     local group_id = vim.api.nvim_create_augroup('alpha_temp', { clear = true })
 
-    vim.api.nvim_create_autocmd({'BufUnload', 'SessionLoadPost'}, {
+    vim.api.nvim_create_autocmd('BufUnload', {
         group = group_id,
         buffer = state.buffer,
+        callback = alpha.close,
+    })
+
+    vim.api.nvim_create_autocmd('SessionLoadPost', {
+        group = group_id,
         callback = alpha.close,
     })
 
